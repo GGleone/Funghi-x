c8fdff0a7fb9d2f432e4934c2f6166b4cfe9dedf
giovanni leone <giovannigregorioleone@gmail.com>
2026-07-06 17:09:08 +0200

    aggiunto roba innutile e commento su label encoding che dovro fare

--- gitpy.py
+++ gitpy.py
@@ -11,9 +11,11 @@ df = pd.read_csv("dataset/mushrooms.csv") #aprire il file csv
 df.info()   #info su colonnee righe
 
 print(df.isnull().sum())    #conta i valori mancanti per ogni colonna (se 0 ovviamente non ne macano quindi il dataset è cmpleto)
-
+print(df.shape)
+print(df.isna().sum())
 print(df.head())
 """ fin qua ho solo controllato se il csv è completo
 e le info riguardo al cvs, se non fosse stato comppleto qua 
 avrei dovuto completarlo in qualche modo e o ripulirlo
 """
+# dovro fare label encding mi sa, per trasformare le lettere in 1 e 0 se true o false
