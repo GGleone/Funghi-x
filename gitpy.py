284b345093c667a1cfe09d6d4fd9e91248410f87
giovanni leone <giovannigregorioleone@gmail.com>
2026-07-08 12:35:53 +0200

    manca label enc per tutti

--- gitpy.py
+++ gitpy.py
@@ -21,7 +21,7 @@ avrei dovuto completarlo in qualche modo e o ripulirlo
 """
 # dovro fare label encding mi sa, per trasformare le lettere in 1 e 0 se true o false
 
-#proviamo con pandas
+#proviamo con pandas.. no
 df_primo = df.copy()
 
 print(df_primo.nunique()) #per vedere il numero di categorie univoche
@@ -29,6 +29,20 @@ print(df_primo.nunique()) #per vedere il numero di categorie univoche
 #ora fare il label del target? che è class 
 #con scikitl
 le = LabelEncoder()
-df_primo["class"] = le.fit_transform(df_primo["class"])
-print(df_primo["class"])
-print(le.classes_) #per sapere a cosa corrispondono ora gli 1 e gli 0
+df_primo["class"] = le.fit_transform(df_primo["class"]) #èil target
+print(df_primo["class"].head(10))
+print(le.classes_) #per sapere a cosa corrispondono ora gli 1 e gli 0 (del target)
+
+#droppare features inutili (con 1 solo valore) 
+# solo veil-type  
+print(df_primo.shape)
+df_primo = df_primo.drop("veil-type", axis=1)
+print(df_primo.shape)
+
+#ora ohe con pandas (one hot encoding, per ottenere piu colonne con sono 2 valori ciascuno)
+df_primo = pd.get_dummies(df_primo)
+print(df_primo.shape)
+#siamo apassati da 22 a 117 colonne
+
+#da rifare il label encoding per tutte le features
+
