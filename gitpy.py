fe589d179bccbd773ba41da880d7037054841a6a
giovanni leone <giovannigregorioleone@gmail.com>
2026-07-09 15:59:42 +0200

    inutile perche regresione lineare non va bene

--- gitpy.py
+++ gitpy.py
@@ -1,10 +1,10 @@
 # cosa rende un fungo velenoso?
 #tanto per lavoro
 
-import numpy as np #inutilizzato per ora
+import numpy as np 
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OrdinalEncoder
+#from sklearn.preprocessing import OrdinalEncoder
 
 # non c'è bisogno di questa riga >>> with open("dataset/mushrooms.csv", "r") as f:
     #file= pd.read_csv(f)
@@ -50,8 +50,32 @@ print(df_primo.shape)
 #oe = OrdinalEncoder()
 #df_primo= oe.fit_transform(df_primo)
 
+"""
 #df_primo.head()       head() non funziona su numpy array
 print(df_primo[:5])
 #tanto per vederlo con pandas convertiamolo e poi facciamo head()
 jim= pd.DataFrame(df_primo)
 print(jim.head())
+"""
+
+
+"""
+class LinearRegressionMia:
+    peso_ =None
+    bias_ = None
+    def fit(self, x, y):
+        x_sum =x.sum()
+        y_sum= y.sum()
+        xy_sum = (x*y).sum()
+        x2_sum= (x*x).sum()
+        n=y.shape[0]
+        self.peso_=(n*xy_sum -x_sum*y_sum)/(n*x2_sum-x_sum*x_sum)
+        self.bias_=(y_sum-self.peso_*x_sum)/n
+
+    def predici(self, x):
+        return self.peso_*x+self.bias_
+    
+lrm=LinearRegressionMia()
+lrm.fit(df_primo[1]df_primo[0])
+print(lrm.predici(True))
+"""  #non funziona perche ho troppe features e dovrei fare regresione lineare multipla e non semplice
